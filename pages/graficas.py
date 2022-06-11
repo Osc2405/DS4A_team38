d6257b8a0a6922596eedba7e444aa0b53644a339
Osc2405 <orosero2405@gmail.com>
2022-06-11 16:33:37 -0500

    Add page  "graficas"

---
+++ pages/graficas.py
@@ -0,0 +1,16 @@
+import dash
+from dash import dcc,html
+import dash_bootstrap_components as dbc
+
+from app import app
+
+## Layout national
+layout=html.Div(
+    children=[
+        html.H1(
+            children="About page"
+        ),
+        html.P("We are interested in searching which are the main problems of deforestation in Colombia. Here we can see explicitly the changes along each year since 1990 to 2020",
+        className="text-justify")
+    ]
+)
