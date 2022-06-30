87bf0af3c0909686060401b8a832a2865a6fe541
Luis Enrique Carreño Herreño <luisenriquecarreno@hotmail.com>
2022-06-29 20:13:15 -0500

    Settings for deploying to Heroku

--- app.py
+++ app.py
@@ -385,5 +385,4 @@ register_callbacks(app)
 
 
 if __name__ == "__main__":
-    #app.run_server(debug=True, host="0.0.0.0", port=8080)
-    app.run_server(debug=True, port=8080)
+    app.run_server(debug=True, host="0.0.0.0", port=8080)
